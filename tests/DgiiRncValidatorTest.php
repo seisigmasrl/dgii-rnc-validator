2bb6ccb3977d02084a5213010b5b3ea4bee17869
Ricardo Vargas <rvargas@seisigma.co>
2023-01-21 00:40:45 -0400

    Updating test with the missing RNC parameter.

--- tests/DgiiRncValidatorTest.php
+++ tests/DgiiRncValidatorTest.php
@@ -9,40 +9,15 @@ test('check if the given string is a valid RNC', function () {
         ->and(DgiiRncValidator::validateRNC("12345678901"))->toBeTrue();
 });
 
-it('can validate a Legal Person RNC value is good', function () {
-    $rnc = DgiiRncValidator::validateFormat("132620951");
-    expect($rnc)->toBeTrue();
-});
-
-it('can validate a Physical person RNC values is good', function () {
-    $rnc = DgiiRncValidator::validateFormat("04800009575");
-    expect($rnc)->toBeTrue();
-});
-
-it('can validate a Dominican official ID number', function () {
-    // Good ID
-    $rnc = DgiiRncValidator::validatePersonId("04800009575");
-    expect($rnc)->toBeTrue();
-
-    // Wrong ID
-    $rnc = DgiiRncValidator::validatePersonId("04800009577");
-    expect($rnc)->toBeFalse();
-});
-
 it('can return the details of an RNC if true', function () {
-    $taxpayer = DgiiRncValidator::check("132620951");
+    $id = "132620951";
+    $taxpayer = DgiiRncValidator::check($id);
     expect($taxpayer)
         ->toBeArray()
-        ->toHaveProperties(['name', 'commercial_name', 'status'])
         ->toMatchArray([
+            "rnc" => $id,
             "name" => "KOI CORPORATION BY SAIKOV SRL",
             "commercial_name" => "KOI CORPORATION BY SAIKOV",
             "status" => "Active"
         ]);
-});
-
-it('can return false if the RNC is invalid', function () {
-    $taxpayer = DgiiRncValidator::check("123456789");
-    expect($taxpayer)
-        ->toBeFalse();
 });
