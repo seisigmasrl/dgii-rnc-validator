cc4d39b6da59cc0d1b696707828af26b7127b51b
Ricardo Vargas <im@ricardovargas.me>
2023-02-10 17:36:21 -0400

    Fixing the Status enum by validating all Status types from the API.

--- src/helpers/Status.php
+++ src/helpers/Status.php
@@ -6,14 +6,18 @@ namespace Seisigma\DgiiRncValidator\helpers;
 
 enum Status: int
 {
+    case DECOMMISSIONED = 3;
     case ACTIVE = 2;
     case INACTIVE = 1;
+    case SUSPENSE = 0;
 
     public function toString(): string
     {
         return match ($this) {
-            self::ACTIVE => 'Active',
-            self::INACTIVE => 'Inactive'
+            self::DECOMMISSIONED => 'DADO DE BAJA',
+            self::ACTIVE => 'ACTIVO',
+            self::INACTIVE => 'INACTIVO',
+            self::SUSPENSE => 'SUSPENDIDO',
         };
     }
 }
